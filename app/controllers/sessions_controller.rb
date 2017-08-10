157648a3da4c88f677e0f014d23fb9537d64a184
samwel <samwuelkinuthia@gmail.com>
2017-08-10 19:24:37 +0300

    checked password and username when logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(username: login_params[:username])
+
+    if user && user.authenticate(login_params[:password])
+      login(user)
+      redirect_back fallback_location: new_session_path, notice; 'logged_in'
+    else
+      flash.now[:notice] = 'Password and Username Do not match. Try Again'
+      render new
+    end
+  end
+
+  private
+
+  def login_params
+    params.require(:session).permit(:username, :password)
+  end
 end
