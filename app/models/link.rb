8a889784f7a88b4b37fc01f8352681da67fcd59e
samwel <samwuelkinuthia@gmail.com>
2017-08-10 22:09:39 +0300

    link and title validation

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,10 @@
 class Link < ApplicationRecord
   belongs_to :user
+
+  validates :title,
+              presence: true,
+              uniqueness: {case_sensitive: false}
+  validates :url,
+              format: {with: %r{\Ahttps?://} },
+              allow_blank: true  
 end
