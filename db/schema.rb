a02b3bd8a8b7c899cc842289c30ab5b9c3e83b48
samwel <samwuelkinuthia@gmail.com>
2017-08-10 22:07:01 +0300

    created table links with foreign key user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170810142522) do
+ActiveRecord::Schema.define(version: 20170810190136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
+    t.text "description"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_links_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 20170810142522) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "links", "users"
 end
