e93bec6fbe78473d47f0a080f5ae62792c19a54b
samwel <samwuelkinuthia@gmail.com>
2017-08-10 18:59:36 +0300

    replaced get with resources for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+  resources :sessions, only: [:new, :create]
   resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
