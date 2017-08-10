f7cd768f24c296cc8f1cebbb8ad2f9bf3b22bd59
samwel <samwuelkinuthia@gmail.com>
2017-08-10 22:28:49 +0300

    called authorize before_action and login user automatically after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :prevent_logged_in_user_access
   def new
     @user = User.new
   end
@@ -6,7 +7,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_back fallback_location: new_user_path
+      login(@user)
+      redirect_to root_path, notice: 'Logged in'
     else
       render :new
     end
