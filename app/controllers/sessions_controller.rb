7d05b0d5362d68a42e9ca6e941987b218cc4f58d
samwel <samwuelkinuthia@gmail.com>
2017-08-10 22:31:11 +0300

    set visibility of logout to only authorized users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,8 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  before_action :prevent_logged_in_user_access, except: :destroy
+  before_action :prevent_unauthorized_user_access, only: :destroy
+  
+  def new; end
 
   def create
     user = User.find_by(username: login_params[:username])
