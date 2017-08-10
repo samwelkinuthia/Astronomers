b39fa14a3dce391328f8545930dc75433919a790
samwel <samwuelkinuthia@gmail.com>
2017-08-10 17:42:39 +0300

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username,
+              presence: true,
+              length: {minimum: 3},
+              uniqueness:{case_sensitive: false}
+  validates :password,
+              length: {minimum: 8}
 end
