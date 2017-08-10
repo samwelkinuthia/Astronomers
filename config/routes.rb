914493767152feae90a3e965d0160cdb3215f0ba
samwel <samwuelkinuthia@gmail.com>
2017-08-10 22:12:28 +0300

    set index route and links resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  get 'links/index'
+  root 'links#index'
 
-  get 'links/new'
-
-  get 'links/show'
-
-  get 'links/edit'
+  resources :links, except: :index
 
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
+  
   resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
