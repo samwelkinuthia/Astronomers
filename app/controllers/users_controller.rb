fc85a919e68dc8ad9afce834b9f41a7025a1a969
samwel <samwuelkinuthia@gmail.com>
2017-08-10 18:52:02 +0300

    defined private, create and new methods with user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_back fallback_location: new_user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:username, :password)
   end
 end
