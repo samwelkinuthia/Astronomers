8ed379fa050bf94f678a67b38729e9dbc7fb9a57
samwel <samwuelkinuthia@gmail.com>
2017-08-10 21:39:04 +0300

    removed extra : before notice

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,16 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(login_params[:password])
       login(user)
-      redirect_back fallback_location: new_session_path, notice; 'logged_in'
+      redirect_back fallback_location: new_session_path, notice: 'logged_in'
     else
       flash.now[:notice] = 'Password and Username Do not match. Try Again'
-      render new
+      render :new
     end
   end
 
   def destroy
     logout
-    redirect_back fallback_location: new_session_path, :notice: 'Logged Out'
+    redirect_back fallback_location: new_session_path, notice: 'Logged Out'
   end
 
   private
