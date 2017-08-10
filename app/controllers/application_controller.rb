2de9910f49c37a8f38d7ff399e268d1df5d633b5
samwel <samwuelkinuthia@gmail.com>
2017-08-10 19:12:23 +0300

    login, logout session methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logout
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def logged_in?
+    current_user.nil ? false : true
+  end
+
+  helper_method :current_user, :logged_in?
 end
