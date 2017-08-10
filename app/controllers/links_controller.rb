27a62abb0a0d328e9c798414202f0db372ccda7f
samwel <samwuelkinuthia@gmail.com>
2017-08-10 22:37:30 +0300

    defined create and private methods for creating new link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,11 +1,23 @@
 class LinksController < ApplicationController
   before_action :prevent_unauthorized_user_access, only: %i[new edit]
 
-  def index; end
+  def new
+    @link = Link.new
+  end
 
-  def new; end
+  def create
+    @link = current_user.links.new(link_params)
 
-  def show; end
+    if @link.save
+      redirect_to root_path, notice: 'Link successfully created'
+    else
+      render :new
+    end
+  end
 
-  def edit; end
+  private
+
+  def link_params
+    params.require(:link).permit(:title, :url, :description)
+  end
 end
