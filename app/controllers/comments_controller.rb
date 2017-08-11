c74a933a3cb4477a1d3bb4396c3ee5c0d8710caa
samwel <samwuelkinuthia@gmail.com>
2017-08-11 13:53:54 +0300

    defined create method and private method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,22 @@
 class CommentsController < ApplicationController
-  def index
+  def index; end
+
+  def edit; end
+
+  def create
+    @link = Link.find_by(id: params[:link_id])
+    @comment = @link.comments.new(user: current_user, body: comment_params[:body])
+
+    if @comment.save
+      redirect_to @link, notice: 'comment created'
+    else
+      redirect_to @link, notice: 'comment not saved'
+    end
   end
 
-  def edit
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
   end
 end
