cfa6ae16310b9c678c90f927fca2fad8a8af1dec
samwel <samwuelkinuthia@gmail.com>
2017-08-11 13:34:12 +0300

    set has_many rltship with comments

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,10 +1,11 @@
 class Link < ApplicationRecord
   belongs_to :user
-
+  has_many :comments
+  
   validates :title,
               presence: true,
               uniqueness: {case_sensitive: false}
   validates :url,
               format: {with: %r{\Ahttps?://} },
-              allow_blank: true  
+              allow_blank: true
 end
