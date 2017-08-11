998498728166af270afde19feee486dd9d98a623
samwel <samwuelkinuthia@gmail.com>
2017-08-11 14:44:28 +0300

    created comemnt_count method

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,11 +1,15 @@
 class Link < ApplicationRecord
   belongs_to :user
   has_many :comments
-  
+
   validates :title,
               presence: true,
               uniqueness: {case_sensitive: false}
   validates :url,
               format: {with: %r{\Ahttps?://} },
               allow_blank: true
+
+  def comment_count
+    comments.length
+  end
 end
