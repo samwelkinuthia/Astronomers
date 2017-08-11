7ad070ffae594cb2ada5668e58b89cdb308a7ac9
samwel <samwuelkinuthia@gmail.com>
2017-08-11 13:47:57 +0300

    nested comments in links and set comment route

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  get 'comments/index'
 
-  get 'comments/edit'
 
   root 'links#index'
 
-  resources :links, except: :index
+  resources :links, except: :index do
+    resources :comments, only: [:create, :edit, :update, :destroy]
+  end
+
+  get '/comments' => 'comments#index'
 
   resources :sessions, only: [:new, :create] do
     delete :destroy, on: :collection
   end
-  
+
   resources :users, only: [:new, :create]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
