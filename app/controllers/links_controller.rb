a8d3be6aa2f9c4d628dbff4b909a5ef38384525f
samwel <samwuelkinuthia@gmail.com>
2017-10-11 16:38:49 +0300

    remove vote for user on clicking downvote

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,8 +2,8 @@ class LinksController < ApplicationController
   before_action :prevent_unauthorized_user_access, only: %i[new edit]
 
   def show
-    @link=Link.find_by(id: params[:id])
-    @comments=@link.comments
+    @link = Link.find_by(id: params[:id])
+    @comments = @link.comments
   end
 
   def index
@@ -52,18 +52,38 @@ class LinksController < ApplicationController
       render :edit
     end
   end
-#upvote method for links
+
+  # upvote method for links
   def upvote
     link = Link.find_by(id: params[:id])
     if current_user.upvoted?(link)
       current_user.remove_vote(link)
+    elsif current_user.downvoted?(link)
+      current_user.remove_vote(link)
+      current_user.upvote(link)
     else
       current_user.upvote(link)
     end
 
     redirect_to root_path
   end
-#downvotes
+
+  # downvotes
+  def downvote
+    link = Link.find_by(id: params[:id])
+
+    if current_user.downvoted?(link)
+      current_user.remove_vote(link)
+    elsif current_user.upvoted?(link)
+      current_user.remove_vote(link)
+      current_user.downvote(link)
+    else
+      current_user.downvote(link)
+    end
+
+    redirect_to root_path
+  end
+
   private
 
   def link_params
