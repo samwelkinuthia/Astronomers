62718a5479f826eedb4c549c301373f186ceacff
samwel <samwuelkinuthia@gmail.com>
2017-10-11 15:52:58 +0300

    set default zero for votes

--- db/migrate/20171011125044_create_votes.rb
+++ db/migrate/20171011125044_create_votes.rb
@@ -3,8 +3,8 @@ class CreateVotes < ActiveRecord::Migration[5.1]
     create_table :votes do |t|
       t.belongs_to :user, foreign_key: true
       t.belongs_to :link, foreign_key: true
-      t.integer :upvote
-      t.integer :downvote
+      t.integer :upvote, default: 0
+      t.integer :downvote, default: 0
 
       t.timestamps
     end
