c299035bbf54675dd32742247fef62d90272f567
samwel <samwuelkinuthia@gmail.com>
2017-10-11 16:34:17 +0300

    method downvote and downvoted? to check for user actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,33 +3,45 @@ class User < ApplicationRecord
   has_many :comments
   has_many :votes
 
-
   validates :username,
-              presence: true,
-              length: {minimum: 3},
-              uniqueness:{case_sensitive: false}
+            presence: true,
+            length: { minimum: 3 },
+            uniqueness: { case_sensitive: false }
   validates :password,
-              length: {minimum: 8}
+            length: { minimum: 8 }
 
   has_many :links, dependent: :destroy
-# identifying owner of particular link
+  # identifying owner of particular link
   def owns_link?(link)
     self == link.user
   end
-# define owner of a particular comment
+
+  # define owner of a particular comment
   def owns_comment?(comment)
     self == comment.user
   end
-#upvote functionality
+
+  # upvote functionality
   def upvote(link)
-    votes.create(upvote: 1, link:link)
+    votes.create(upvote: 1, link: link)
   end
-#checks if user has already voted on a link
+
+  # checks if user has already voted on a link
   def upvoted?(link)
     votes.exists?(upvote: 1, link: link)
   end
-#remove a vote on a link
+
+  # remove a vote on a link
   def remove_vote(link)
     votes.find_by(link: link).destroy
+  end
+
+  # DOWNVOTE FUNCTIONALITY
+  def downvote(link)
+    votes.create(downvote: 1, link: link)
+  end
+
+  def downvoted?(link)
+    votes.exists?(downvote: 1, link: link)
   end
 end
