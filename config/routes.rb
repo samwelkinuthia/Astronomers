fea0feff7f6eb16ef73d4cce7a9017e034127479
samwel <samwuelkinuthia@gmail.com>
2017-10-11 16:32:39 +0300

    downvote route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :links, except: :index do
     resources :comments, only: [:create, :edit, :update, :destroy]
     post :upvote, on: :member
+    post :downvote, on: :member
   end
 
   get '/comments' => 'comments#index'
