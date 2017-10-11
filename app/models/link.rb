61d9fdf25fc0789a3ee271ae012ad4cd679f6921
samwel <samwuelkinuthia@gmail.com>
2017-10-11 15:54:02 +0300

    set has_many with votes

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :votes
 
   validates :title,
               presence: true,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :comments
+  has_many :votes
+
+  
   validates :username,
               presence: true,
               length: {minimum: 3},
