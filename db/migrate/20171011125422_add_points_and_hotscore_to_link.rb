37ac44d63b3664a46b86330c7095d08bc85c0751
samwel <samwuelkinuthia@gmail.com>
2017-10-11 15:56:37 +0300

    default point 1, default hot is 0

--- db/migrate/20171011125422_add_points_and_hotscore_to_link.rb
+++ db/migrate/20171011125422_add_points_and_hotscore_to_link.rb
@@ -1,6 +1,6 @@
 class AddPointsAndHotscoreToLink < ActiveRecord::Migration[5.1]
   def change
-    add_column :links, :points, :integer
-    add_column :links, :hot_score, :float
+    add_column :links, :points, :integer, default: 1
+    add_column :links, :hot_score, :float, default: 0
   end
 end
